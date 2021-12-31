869d21d3b38929af6d3b3d06a9b782bacb10eefb
God_sihas <sabeywickra@gmail.com>
2021-12-31 12:39:51 +0530

    Added API routes

--- src/ethereum/auction.js
+++ src/ethereum/auction.js
@@ -1,6 +1,9 @@
 import web3 from "./web3";
 import Auction from "../../build/contracts/Auction.json";
 
+// * Get time to start in hours from currentBlock ((<startBlock>-<9908958>)*15)/60/60
+// * Get time to end in hours from startBlock ((<endBlock>-<startBlock>)*15)/60/60
+
 const auction = (address) => {
   return new web3.eth.Contract(Auction.abi, address);
 };
